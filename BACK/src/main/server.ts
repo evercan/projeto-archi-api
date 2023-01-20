28e42d2cb3c9439d4be923de633366217b1299b2
Everton da Silva <evercan@bol.com.br>
2023-01-20 14:44:18 -0300

    seetings for .env & server

--- BACK/src/main/server.ts
+++ BACK/src/main/server.ts
@@ -4,16 +4,10 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper/mongo-helper
 require('dotenv').config()
 import app from './config/app'
 
-const port = process.env.PORT || 5000
-    app.listen(port, () => {
-      console.log('Server running at ' + port)
+MongoHelper.connect(process.env.MONGO_URL)
+  .then(async () => {   
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running...')
     })
-
-// MongoHelper.connect(process.env.MONGO_URL)
-//   .then(async () => {
-//     const app = (await import('./config/app')).default
-//     app.listen(process.env.PORT || 5000, () => {
-//       console.log('Server running...')
-//     })
-//   })
-//   .catch(console.error)
+  })
+  .catch(console.error)
