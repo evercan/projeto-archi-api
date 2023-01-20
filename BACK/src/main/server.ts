64332ee5c8b823c9f8fa715f281cec0406a617a2
Everton da Silva <evercan@bol.com.br>
2023-01-20 13:28:11 -0300

    modify info

--- BACK/src/main/server.ts
+++ BACK/src/main/server.ts
@@ -4,21 +4,16 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper/mongo-helper
 require('dotenv').config()
 import app from './config/app'
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-// const Cat = mongoose.model('Cat', { name: String });
-
-// const port = process.env.PORT || 5000
-//     app.listen(port, () => {
-//       console.log('Server running at ' + port)
-//     })
-
-MongoHelper.connect(process.env.MONGO_URL)
-  .then(async () => {
-    const app = (await import('./config/app')).default
-    app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running...')
+const port = process.env.PORT || 5000
+    app.listen(port, () => {
+      console.log('Server running at ' + port)
     })
-  })
-  .catch(console.error)
+
+// MongoHelper.connect(process.env.MONGO_URL)
+//   .then(async () => {
+//     const app = (await import('./config/app')).default
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log('Server running...')
+//     })
+//   })
+//   .catch(console.error)
